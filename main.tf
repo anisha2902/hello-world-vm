8b566fbf096c0422208a2975796b0f9ef4907c01
anisha2902 <154617956+anisha2902@users.noreply.github.com>
2026-01-30 14:32:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ module "gcp_vm" {
   source = "git::https://github.com/anisha2902/module-gcp-vm.git//modules/gcp-vm?ref=main"
   vm_name       = var.vm_name
   machine_type   = var.vm_size
+  region = var.region
   zone = var.zone
 }
